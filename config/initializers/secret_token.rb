3419d073213e995c50a38dbd57c721354edc4d96
Jianhong Liu <liujianhong1986@gmail.com>
2014-03-22 12:03:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itam::Application.config.secret_key_base = 'db1edc98340604e0195d0e497ecab1fa34ffc585444143aba5bd0ccb807bbd777a7fa23883aca903413c4c1272661567d4a649be5129be601789798f57622832'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Itam::Application.config.session_store :cookie_store, key: '_itam_session'
