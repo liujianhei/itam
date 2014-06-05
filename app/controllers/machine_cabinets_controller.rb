7fb881f598d35fa189263516c454b0129200fccc
Jianhong Liu <jianheiliu@gmail.com>
2014-06-05 23:15:46 +0800

    add some machine_cabinets controller and view

--- app/controllers/machine_cabinets_controller.rb
+++ app/controllers/machine_cabinets_controller.rb
@@ -1,5 +1,29 @@
 class MachineCabinetsController < ApplicationController
+
+  def index
+    @machine_cabinets = MachineCabinet.all
+  end
+
   def new
     @machine_cabinet=MachineCabinet.new
   end
+
+  def show
+    @machine_cabinet=MachineCabinet.find(params[:id])
+  end
+
+  def create
+    @machine_cabinet = MachineCabinet.new(machine_cabinet_params)
+    if @machine_cabinet.save
+      redirect_to @machine_cabinet
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def machine_cabinet_params
+      params.require(:machine_cabinet).permit(:number)
+    end
 end
