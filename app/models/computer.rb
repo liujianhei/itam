b19798c0cec1953d95423f9c1ecdadb8fd9a4078
jianhong.liu <jianhong.liu@b5m.com>
2014-06-11 20:40:36 +0800

    change location model

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -3,6 +3,7 @@ class Computer < ActiveRecord::Base
   belongs_to :model
   belongs_to :sa
   belongs_to :switch
+  belongs_to :location
   has_and_belongs_to_many :projects
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
